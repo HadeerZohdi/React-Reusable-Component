ded8da7e3905a980c3dc01ab8a3e7a6a4929f058
Hadeer.zohdi <hadeer.zohdi@gmail.com>
2023-01-06 19:09:58 +0200

    update

--- reusable-card/src/pages/Home/partials/listCards.js
+++ reusable-card/src/pages/Home/partials/listCards.js
@@ -1,9 +1,26 @@
 import React from "react";
 import Card from "../../../components/Card";
 import modal from "../../../assets/model3.jpg";
+import useHomePage from "../hooks/useHomePage";
+import "../style.css";
 
 const ListCard = () => {
-  return <div>listtttttt</div>;
+  const { ProductList } = useHomePage();
+
+  return (
+    <div className="list">
+      {ProductList.map((item) => (
+        <Card
+          key={item.id}
+          title={item.title}
+          description={item.description}
+          image={item.image}
+          price={item.price}
+          isProduct={true}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default ListCard;
